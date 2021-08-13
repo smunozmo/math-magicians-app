7e357a76b6e318bbd7f5908b5fcc7e5a1ee015d2
Sebastián M <smunozmo@gmail.com>
2021-08-13 12:01:22 -0500

    Add calculation and operation.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,26 @@
 import React from 'react';
+// import operate from './operate';
+import calculate from './calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  UpdateObj = (event) => {
+    const newState = calculate(this.state, event.target.value);
+    this.setState(newState);
+    const displayTotal = document.querySelector('form');
+    if (newState.next !== null) {
+      displayTotal.innerText = newState.next;
+    } else {
+      displayTotal.innerText = newState.total;
+    }
   }
 
   render() {
@@ -11,38 +28,36 @@ class Calculator extends React.Component {
       <div className="App">
         <table>
           <td>
-            <input type="text" name="input" value="0" className="display" />
+            <form type="text" value="0" className="display">0</form>
             <br />
-            <input type="button" name="clear" value="c" />
-            <input type="button" name="sign" value="+/-" />
-            <input type="button" name="percent" value="%" />
-            <input type="button" name="divided" value="/" className="orange" />
+            <button type="button" value="AC" onClick={this.UpdateObj}>AC</button>
+            <button type="button" value="+" onClick={this.UpdateObj}>+/-</button>
+            <button type="button" value="%" onClick={this.UpdateObj}>%</button>
+            <button type="button" value="÷" className="orange" onClick={this.UpdateObj}>÷</button>
             <br />
-            <input type="button" name="seven" value="7" />
-            <input type="button" name="eight" value="8" />
-            <input type="button" name="nine" value="9" />
-            <input type="button" name="times" value="x" className="orange" />
+            <button type="button" value="7" onClick={this.UpdateObj}>7</button>
+            <button type="button" value="8" onClick={this.UpdateObj}>8</button>
+            <button type="button" value="9" onClick={this.UpdateObj}>9</button>
+            <button type="button" value="x" onClick={this.UpdateObj} className="orange">x</button>
             <br />
-            <input type="button" name="four" value="4" />
-            <input type="button" name="five" value="5" />
-            <input type="button" name="six" value="6" />
-            <input type="button" name="minus" value="-" className="orange" />
+            <button type="button" value="4" onClick={this.UpdateObj}>4</button>
+            <button type="button" value="5" onClick={this.UpdateObj}>5</button>
+            <button type="button" value="6" onClick={this.UpdateObj}>6</button>
+            <button type="button" value="-" onClick={this.UpdateObj} className="orange">-</button>
             <br />
-            <input type="button" name="one" value="1" />
-            <input type="button" name="two" value="2" />
-            <input type="button" name="three" value="3" />
-            <input type="button" name="plus" value="+" className="orange" />
+            <button type="button" value="1" onClick={this.UpdateObj}>1</button>
+            <button type="button" value="2" onClick={this.UpdateObj}>2</button>
+            <button type="button" value="3" onClick={this.UpdateObj}>3</button>
+            <button type="button" value="+" onClick={this.UpdateObj} className="orange">+</button>
             <br />
-            <input type="button" name="zero" value="0" className="zero" />
-            <input type="button" name="dot" value="." />
-            <input type="button" name="equals" value="=" className="orange" />
+            <button type="button" value="0" onClick={this.UpdateObj} className="zero">0</button>
+            <button type="button" value="." onClick={this.UpdateObj}>.</button>
+            <button type="button" value="=" onClick={this.UpdateObj} className="orange">=</button>
             <br />
           </td>
         </table>
       </div>
-
     );
   }
 }
-
 export default Calculator;
